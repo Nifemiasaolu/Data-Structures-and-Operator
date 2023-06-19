0a49f60604c5ae17f42f3e0a1dabdc2bc41c153a
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-06-19 17:08:26 +0100

    Modern Operator

--- script.js
+++ script.js
@@ -54,8 +54,24 @@ const restaurant = {
   },
 };
 
-///////////////////////////////////////////////
-// LOGICAL ASSIGNMENT OPERATOR 
+//////////////////////////////////////////////////
+//================= OPTIONAL CHAINING(?.) =================
+
+
+
+////////////////////////////////////////////////
+//==================== LOOPING ARRAYS: THE FOR-OF LOOP ====================
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu) console.log(item);
+
+for(const [i, el] of menu.entries()) {
+  // console.log(`${i}: ${el}`);
+}
+
+
+//////////////////////////////////////////////////////
+//============== LOGICAL ASSIGNMENT OPERATOR ==============
 const rest1 = {
   name: 'Capri',
   numGuests: 0
@@ -67,15 +83,6 @@ const rest2 = {
 
 };
 
-////////////////////////////////////////////////
-//==================== LOOPING ARRAYS: THE FOR-OF LOOP ====================
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-
-for(const item of menu) console.log(item);
-
-for(const [i, el] of menu.entries()) {
-  // console.log(`${i}: ${el}`);
-}
 
 
 //////////////////////////////////////////////////////
