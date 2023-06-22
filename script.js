face84cd0721039b712d978186705232566c8e4d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-06-22 17:07:01 +0100

    Working With Strings

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ console.log(announcement.replace(/door/g, 'gate')); //Using Regualar Expression 
 
 const plane = 'Airbus A320';
 console.log(plane.includes('A320'));
-console.log(plane.includes('B737'));
+// console.log(plane.includes('B737'));
 // console.log(plane.startsWith('Air'));
 
 if(plane.startsWith('Airbus') && plane.endsWith('neo')) {
