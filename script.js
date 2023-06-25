1a5a5a7d917e6bf9a19e15a35fe36a1f91b2a8b2
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-06-25 22:54:46 +0100

    Data Structure

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
   // `_Delayed_Departure;fao93766109;txl2133758440;11:25
   // +_Arrival;bru0943384722;fao93766109;11:45
   // +_Delayed_Arrival;hel7439299980;fao93766109;12:05
-  // +_Departure;fao93766109;lis2323639855;12:30`;;
+  // +_Departure;fao93766109;lis2323639855;12:30`;
 
 // Data needed for first part of the sections
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
